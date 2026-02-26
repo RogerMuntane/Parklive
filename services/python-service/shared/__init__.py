2ec7068b40cffc46aaf7d26c1e9acc3a32181486
RogerMuntane <rogermuntane123@gmail.com>
2026-02-26 20:43:14 +0100

    temp

--- services/python-service/shared/__init__.py
+++
@@ -1,7 +0,0 @@
-# shared/__init__.py
-"""
-Mòdul compartit amb utilitats comunes
-"""
-from .serializers import serialize_value, serialize_row, serialize_rows
-
-__all__ = ['serialize_value', 'serialize_row', 'serialize_rows']
